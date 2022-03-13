f133b1c2b1283070836188f5984c58e8db4dd1ed
Pedro-Marcos1223 <pedromarcospdm@gmail.com>
2022-03-13 17:09:40 -0300

    @Pedro-Marcos1223 [Projeto Criado]

---
+++ backend/src/main/java/com/devsuperior/dsmovieprofissional/DsmovieprofissionalApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieprofissional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieprofissionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieprofissionalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovieprofissional/DsmovieprofissionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieprofissional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieprofissionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
